5c7ffcfb7aab0e73ef00343c4c6b5736a12ec03f
tirra <tirra>
2008-08-20 21:12:09 +0000

    added pure CString stream implementation. not tested yet. will be updated with TODO: notes

--- redleaf/redleaf/src/include/liballoc/scstr.h
+++ redleaf/redleaf/src/include/liballoc/scstr.h
@@ -20,30 +20,72 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */ 
 
-#ifndef __CSSTR_H__
-#define __CSSTR_H__
+#ifndef __SCSTR_H__
+#define __SCSTR_H__
 
 #include <sys/types.h>
 
+/*
+ * TODO: 
+ * ** rewrite all to be more kindly,
+ *    add counter to scstr_t - counter of reallocations, 
+ *    and if we're making many remappings - increase chunk size 
+ *    (somebody's fucking deal)
+ * ** Create a pull of stream to make a choose from it's to avoid
+ *    many mmap() calls (stupidity-safe)
+ */
+
 typedef struct __scstr_t {
-  size_t amount;
-  int off_size;
-  size_t used;
-  size_t size;
-  void *pp;
+  size_t amount; /*chunk size*/
+  int off_size; /*overdraft size*/
+  size_t used; /*really used*/
+  size_t size; /*real size*/
+  void *pp; /*real pointer to the data*/
 } scstr_t;
 
+/*======pure CString stream operations=====*/
+/**
+ * scstr_new(): create new CString stream
+ * (def_amount: size_t) default chunk size
+ * return: pointer to a newly allocated stream
+ */
 scstr_t *scstr_new(size_t def_amount);
-
+/**
+ * scstr_add(): adds cstring to stream
+ * (sc: *scstr_t) pointer to existing CString stream
+ * (scstr: *char) pointer to cstring
+ * return: pointer to stream
+ * >NOTE: always assign your pointer to this
+ *        function return, because pointer to
+ *        stream can be changed
+ */
 scstr_t *scstr_add(scstr_t *sc,char *cstr);
-
+/**
+ * scstr_addn(): adds cstring to stream (with lenght limit)
+ * (sc: *scstr_t) pointer to existing CString stream
+ * (scstr: *char) pointer to cstring
+ * (len: int) lenght to add
+ * return: pointer to stream
+ * >NOTE: always assign your pointer to this
+ *        function return, because pointer to
+ *        stream can be changed
+ */
 scstr_t *scstr_addn(scstr_t *sc,char *cstr,int len);
-
+/**
+ * scstr_len(): get a lenght of fullfilled data
+ * (sc: *scstr_t) pointer to a stream
+ * return: lenght of actually filled data
+ */
 size_t scstr_len(scstr_t *sc);
 
+/*clear stream data*/
 int scstr_clear(scstr_t *sc);
 
+/*remove stream itself*/
 int scstr_remove(scstr_t *sc);
 
-#endif
+/*get a pointer to stream data*/
+char *scstr_get(scstr_t *sc);
+
+#endif /*__SCSTR_H__*/
 
--- redleaf/redleaf/src/liballoc/scstr.c
+++ redleaf/redleaf/src/liballoc/scstr.c
@@ -29,10 +29,21 @@
 #include <sys/types.h>
 
 #include <liballoc/scstr.h>
+#include <libdata/usrtc.h> 
 
 #define DEF_CSSTRAMOUNT  256
 #define DEF_ATHRESHOLD   128
 #define DEF_ALIGN        DEF_CSSTRAMOUNT/32
+
+/*
+ * TODO: 
+ * ** rewrite all to be more kindly,
+ *    add counter to scstr_t - counter of reallocations, 
+ *    and if we're making many remappings - increase chunk size 
+ *    (somebody's fucking deal)
+ * ** Create a pull of stream to make a choose from it's to avoid
+ *    many mmap() calls (stupidity-safe)
+ */
 
 scstr_t *scstr_new(size_t def_amount)
 {
@@ -51,7 +62,7 @@ scstr_t *scstr_new(size_t def_amount)
 
   pp=mmap(0,def_amount+odf,PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANON,-1,(off_t)0);
   if(pp==MAP_FAILED) {
-    fprintf(stderr,"mmap failed, aborting.\n");
+    fprintf(stderr,"[EE] mmap failed, aborting.\n");
     perror("mmap:");
     return NULL;
   }
@@ -61,36 +72,112 @@ scstr_t *scstr_new(size_t def_amount)
   r->used=0;
   r->amount=def_amount;
   r->pp=(void*)((char*)pp+odf);
+  memset(r->pp,'\0',def_amount);
 
   return r;
 }
 
 scstr_t *scstr_add(scstr_t *sc,char *cstr)
 {
+  int len,i;
+  void *n; 
+  scstr_t *re;
+
+  if(!sc) {
+    fprintf(stderr,"[EE] Given CString stream is nil.\n");
+    return NULL;
+  }
+
+  len=strlen(cstr);
+  if((sc->size)-(sc->used)<len+1) {
+    if(len+1>sc->amount) { /*oops, let's reassign new stream in case of somebody's stupidity*/
+      scstr_t *nsc=scstr_new((len+1)*4);
+      /*ok, let's copy old content*/
+      nsc->used=sc->used;
+      strcpy((char*)nsc->pp,(const char*)sc->pp);
+      scstr_remove(sc);
+      return scstr_add(nsc,cstr);
+    }
+    /*let's reallocate*/
+    i=sc->size/sc->amount;
+    n=mmap(0,((i+1)*sc->amount)+sc->off_size,PROT_READ | PROT_WRITE,MAP_PRIVATE | MAP_ANON,-1,(off_t)0);
+    if(n==MAP_FAILED) {
+      fprintf(stderr,"[EE] mmap failed, aborting.\n");
+      perror("mmap:");
+      return sc;
+    }
+    memset(n,'\0',((i+1)*sc->amount)+sc->off_size);
+    memcpy(n,(const void*)sc,sc->off_size+sc->used); /*copy all contents*/
+    re=(scstr_t*)n;
+    re->pp=(void*)((char*)n+sc->off_size); /*reassign new pointer*/
+    scstr_remove(sc); /*unmap all data*/
+    strcat((char*)re->pp,cstr); /*actual cat*/
+    re->used+=len;
+    return re;
+  } else { /*simply concatenate given string*/
+    strcat((char*)sc->pp,cstr);
+    sc->used+=len;
+  }
+
   return sc;
 }
 
 scstr_t *scstr_addn(scstr_t *sc,char *cstr,int len)
 {
+  char ss=cstr[len+1]; /*ugly hack,we'll change cstring for time being*/
+
+  cstr[len+1]='\0'; /*limit cstring*/
+  sc=scstr_add(sc,cstr); /*do actual add*/
+  cstr[len+1]=ss; /*restore*/
+
   return sc;
 }
 
 size_t scstr_len(scstr_t *sc)
 {
   if(!sc) {
-    fprintf(stderr,"Given CString stream is nil.\n");
+    fprintf(stderr,"[EE] Given CString stream is nil.\n");
     return 0;
   }
-  else return sc->used;
+  else return sc->used; /*you know*/
 }
 
 int scstr_clear(scstr_t *sc)
 {
+  if(!sc) {
+    fprintf(stderr,"[EE] Given CString stream is nil.\n");
+    return -1;
+  }
+
+  memset(sc->pp,'\0',sc->size); /*simply set alls to zero*/
+  sc->used=0;
+
   return 0;
 }
 
 int scstr_remove(scstr_t *sc)
 {
+  if(!sc) {
+    fprintf(stderr,"[EE] Given CString stream is nil.\n");
+    return -1;
+  }
+
+  if(munmap((void*)sc,sc->size+sc->off_size)) {
+    fprintf(stderr,"[EE] Unmapping failed.\n");
+    perror("munmap:");
+    return -1;
+  }
+
   return 0;
+}
+
+char *scstr_get(scstr_t *sc)
+{
+  if(!sc) {
+    fprintf(stderr,"[EE] Given CString stream is nil.\n");
+    return NULL;
+  }
+
+  return (char*)sc->pp; /*return pointer to real data*/
 }
 
